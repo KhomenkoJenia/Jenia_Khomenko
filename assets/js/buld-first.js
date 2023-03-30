25d3acf875b20ccba3469022aa811ea24ac4e0c1
JeniaKhomenko <jeniaaajeniaaa@gmail.com>
2023-03-31 00:52:47 +0300

    build

--- assets/js/buld-first.js
+++ assets/js/buld-first.js
@@ -18,4 +18,38 @@ buildTower.forEach((item) => {
 	});
 });
 
-const flatsTower = document.querySelectorAll(".flats-build");
+const flats = document.querySelectorAll(".flats-build");
+
+flats.forEach((element) => {
+	const flatAtr = element.getAttribute("data-status");
+	const flatStatus = element.querySelector(".flat-status");
+	const flatsStatusBg = element.querySelector(".flat-number-bg");
+	if (flatAtr == "sold") {
+		flatStatus.innerHTML = "Продано";
+		flatsStatusBg.style.fill = "#ff0400d4";
+		element.classList.add("sold");
+	} else if (flatAtr == "booked") {
+		flatsStatusBg.style.fill = "#f3f708";
+		flatStatus.innerHTML = "Заброньованно";
+		element.classList.add("booked");
+	} else if (flatAtr == "action") {
+		flatStatus.innerHTML = "Акція";
+		flatsStatusBg.style.fill = "#0ce3ff";
+		element.setAttribute("data-bs-target", "#exampleModalToggle2");
+		element.setAttribute("data-bs-toggle", "modal");
+	}
+});
+
+const buildRoom = document.querySelectorAll(".room-build");
+const roomName = document.querySelector("#room-name");
+const roomSquare = document.querySelector("#room-square");
+
+buildRoom.forEach((room) => {
+	room.addEventListener("mouseover", () => {
+		const square = room.getAttribute("data-name-room");
+		const name = room.getAttribute("data-squere");
+
+		roomName.innerHTML = name;
+		roomSquare.innerHTML = square;
+	});
+});
